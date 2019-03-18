d3d770c7b5dbe86e66c5b68277d5ed62465730cc
Manuel Montenegro <manumonti@gmail.com>
2019-03-18 11:17:10 +0100

    Updated to nRF SDK v 15.3.0

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# nRF52840_NB-IoT
+# nRF52840_NB-IoT_AT_Terminal
 
 This application stablish a bypass connection between USB UART and Arduino serial pins UART, i.e. D0 and D1 pins.
 
@@ -6,4 +6,36 @@ This is used in order to manage NB-IoT module directly through AT commands.
 
 This code has been developed for Nordic Semiconductor nRF52840 DK and NB-IoT Arduino shield. 
 
-Tested NB-IoT shileds: AVNET BG96 and Sodaq Sara N211.
+Tested NB-IoT shileds: AVNET BG96.
+
+## Requeriments
+
+This project uses:
+-   nRF5 SDK version 15.3.0
+-   nRF52840 PDK (PCA10056)
+
+## Get started
+
+You can find a hex folder with the **precompiled application** for PCA10056.
+
+In order to stablish serial connection with your PC, you can use PUTTY if you are a Windows user or GNU Screen if you are a Linux user.
+
+**PUTTY connection parameters:**
+
+    Baud rate: 115.200
+    8 data bits
+    1 stop bit
+    No parity
+    HW flow control: None
+
+**GNU screen**
+
+    sudo screen /dev/ttyACM0 115200
+    
+Where */dev/ttyACM0* is the nRF52840 device. You can know what is the path of your nRF52840 connecting it and executing *dmesg* on UNIX terminal.
+
+## Compiling the applications
+
+If you want to compile the project, you can use SEGGER Embedded Studio for ARM. Put the downloaded folder into 
+	
+	\nRF5_SDK_15.3.0\apps\
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /***************************************************************************************/
 /*
- * nRF52840_NB-IoT
- * Created by Manuel Montenegro, Feb 05, 2019.
+ * nRF52840-AT_terminal
+ * Created by Manuel Montenegro, Mar 14, 2019.
  * Developed for MOTAM project.
  *
  *  This application stablish a bypass connection between USB UART and Arduino serial 
@@ -16,7 +16,6 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
-
 #include "nrf.h"
 #include "nrf_drv_clock.h"
 #include "nrf_gpio.h"
@@ -26,8 +25,6 @@
 #include "nrf_drv_power.h"
 #include "nrf_serial.h"
 #include "app_timer.h"
-
-
 #include "app_error.h"
 #include "app_util.h"
 #include "boards.h"
@@ -48,116 +45,89 @@
 #define ARDUINO_RX_PIN          NRF_GPIO_PIN_MAP(1,1)
 #define ARDUINO_TX_PIN          NRF_GPIO_PIN_MAP(1,2)
 
+
 static void sleep_handler(void)
 {
-   __WFE();
-   __SEV();
-   __WFE();
+    __WFE();
+    __SEV();
+    __WFE();
 }
 
-// UART0: USB UART instance
 NRF_SERIAL_DRV_UART_CONFIG_DEF(m_uarte0_drv_config,
-                     RX_PIN_NUMBER, TX_PIN_NUMBER,
-                     RTS_PIN_NUMBER, CTS_PIN_NUMBER,
-                     NRF_UART_HWFC_DISABLED, NRF_UART_PARITY_EXCLUDED,
-                     NRF_UART_BAUDRATE_115200,
-                     UART_DEFAULT_CONFIG_IRQ_PRIORITY);
+                      RX_PIN_NUMBER, TX_PIN_NUMBER,
+                      RTS_PIN_NUMBER, CTS_PIN_NUMBER,
+                      NRF_UART_HWFC_DISABLED, NRF_UART_PARITY_EXCLUDED,
+                      NRF_UART_BAUDRATE_115200,
+                      UART_DEFAULT_CONFIG_IRQ_PRIORITY);
 
-// UNART1: NB-IoT module UART instance
 NRF_SERIAL_DRV_UART_CONFIG_DEF(m_uarte1_drv_config,
-                     ARDUINO_RX_PIN, ARDUINO_TX_PIN,
-                     RTS_PIN_NUMBER, CTS_PIN_NUMBER,
-                     NRF_UART_HWFC_DISABLED, NRF_UART_PARITY_EXCLUDED,
-                     NRF_UART_BAUDRATE_115200,
-                     UART_DEFAULT_CONFIG_IRQ_PRIORITY);
+                      ARDUINO_RX_PIN, ARDUINO_TX_PIN,
+                      RTS_PIN_NUMBER, CTS_PIN_NUMBER,
+                      NRF_UART_HWFC_DISABLED, NRF_UART_PARITY_EXCLUDED,
+                      NRF_UART_BAUDRATE_115200,
+                      UART_DEFAULT_CONFIG_IRQ_PRIORITY);
+
 
 NRF_SERIAL_QUEUES_DEF(serial0_queues, SERIAL_FIFO_TX_SIZE, SERIAL_FIFO_RX_SIZE);
 NRF_SERIAL_QUEUES_DEF(serial1_queues, SERIAL_FIFO_TX_SIZE, SERIAL_FIFO_RX_SIZE);
 
+
 NRF_SERIAL_BUFFERS_DEF(serial0_buffs, SERIAL_BUFF_TX_SIZE, SERIAL_BUFF_RX_SIZE);
 NRF_SERIAL_BUFFERS_DEF(serial1_buffs, SERIAL_BUFF_TX_SIZE, SERIAL_BUFF_RX_SIZE);
 
+
 NRF_SERIAL_CONFIG_DEF(serial0_config, NRF_SERIAL_MODE_DMA,
-                     &serial0_queues, &serial0_buffs, NULL, sleep_handler);
+                      &serial0_queues, &serial0_buffs, NULL, sleep_handler);
 NRF_SERIAL_CONFIG_DEF(serial1_config, NRF_SERIAL_MODE_DMA,
-                     &serial1_queues, &serial1_buffs, NULL, sleep_handler);
+                      &serial1_queues, &serial1_buffs, NULL, sleep_handler);
+
 
 NRF_SERIAL_UART_DEF(serial0_uarte, 0);
 NRF_SERIAL_UART_DEF(serial1_uarte, 1);
 
 
-// ======== Board initialization ========
-
-// Clock initialization
-static void clock_init (void)
+int main(void)
 {
     ret_code_t ret;
 
     ret = nrf_drv_clock_init();
     APP_ERROR_CHECK(ret);
-
-    nrf_drv_clock_lfclk_request(NULL);
-    while(!nrf_drv_clock_lfclk_is_running())
-    {
-        /* Just waiting */
-    }
-}
-
-// Power module initialization
-static void power_init (void) 
-{
-    ret_code_t ret;
     ret = nrf_drv_power_init(NULL);
     APP_ERROR_CHECK(ret);
-}
 
-// Timers initialization
-static void timers_init (void)
-{
-    ret_code_t ret;
+    nrf_drv_clock_lfclk_request(NULL);
     ret = app_timer_init();
     APP_ERROR_CHECK(ret);
-}
 
-// Serial initialization
-static void serial_init (void)
-{
-   ret_code_t ret;
-   ret = nrf_serial_init(&serial0_uarte, &m_uarte0_drv_config, &serial0_config);
-   APP_ERROR_CHECK(ret);
-   ret = nrf_serial_init(&serial1_uarte, &m_uarte1_drv_config, &serial1_config);
-   APP_ERROR_CHECK(ret);
-}
+    ret = nrf_serial_init(&serial0_uarte, &m_uarte0_drv_config, &serial0_config);
+    APP_ERROR_CHECK(ret);
 
+    ret = nrf_serial_init(&serial1_uarte, &m_uarte1_drv_config, &serial1_config);
+    APP_ERROR_CHECK(ret);
 
-int main(void)
-{
-    clock_init();
-    power_init();
-    timers_init();
-    serial_init();
-
-    char charFromSerial0;
-    char charFromSerial1;
-    size_t countFromSerial0;
-    size_t countFromSerial1;
+    char c;
 
     while (true)
     {
-        nrf_serial_read(&serial0_uarte, &charFromSerial0, sizeof(charFromSerial0), &countFromSerial0, 10);
-        while ( countFromSerial0!= 0)
+        
+        ret = nrf_serial_read(&serial0_uarte, &c, sizeof(c), NULL, 10);
+        while (ret == NRF_SUCCESS)
         {
-            nrf_serial_write(&serial1_uarte, &charFromSerial0, sizeof(charFromSerial0), NULL, 0);
-            nrf_serial_flush(&serial1_uarte, 0);
-            nrf_serial_read(&serial0_uarte, &charFromSerial0, sizeof(charFromSerial0), &countFromSerial0, 0);
+            nrf_serial_write(&serial1_uarte, &c, sizeof(c), NULL, 0);
+            ret = nrf_serial_read(&serial0_uarte, &c, sizeof(c), NULL, 10);
+            if (ret != NRF_SUCCESS) {
+                nrf_serial_flush(&serial1_uarte, 0);
+            }
         }
 
-        nrf_serial_read(&serial1_uarte, &charFromSerial1, sizeof(charFromSerial1), &countFromSerial1, 10);
-        while (countFromSerial1 != 0)
+        ret = nrf_serial_read(&serial1_uarte, &c, sizeof(c), NULL, 10);
+        while (ret == NRF_SUCCESS)
         {
-            nrf_serial_write(&serial0_uarte, &charFromSerial1, sizeof(charFromSerial1), NULL, 0);
-            nrf_serial_flush(&serial0_uarte, 0);
-            nrf_serial_read(&serial1_uarte, &charFromSerial1, sizeof(charFromSerial1), &countFromSerial1, 0);
+            nrf_serial_write(&serial0_uarte, &c, sizeof(c), NULL, 0);
+            ret = nrf_serial_read(&serial1_uarte, &c, sizeof(c), NULL, 10);
+            if (ret != NRF_SUCCESS) {
+                nrf_serial_flush(&serial0_uarte, 0);
+            }
         }
     }
 }
